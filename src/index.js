1f8c0cb3b95769c6776dbc725b133bc4bcee8c51
Jackie0918 <jiachi.wu@gmail.com>
2021-03-17 15:04:36 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import WeatherApp from "./WeatherApp";
+
+// 這支 CSS 檔的樣式會作用到全域
+import "./styles.css";
+
+function App() {
+  return <WeatherApp />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
